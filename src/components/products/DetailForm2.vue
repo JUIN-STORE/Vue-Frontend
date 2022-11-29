24255ca6c51147372be2b0e14b433630b4f6d36e
jduckling1024 <jduck1024@naver.com>
2022-11-30 07:16:09 +0900

    PRODUCT-7 기타 페이지 연결

--- src/components/products/DetailForm2.vue
+++ src/components/products/DetailForm2.vue
@@ -4,8 +4,12 @@
       <div class="row px-xl-5">
         <div class="col-12">
           <nav class="breadcrumb bg-light mb-30">
-            <a class="breadcrumb-item text-dark" href="#">Home</a>
-            <a class="breadcrumb-item text-dark" href="#">Shop</a>
+            <router-link to="/" class="breadcrumb-item text-dark"
+              >Home</router-link
+            >
+            <router-link to="/products" class="breadcrumb-item text-dark"
+              >Shop</router-link
+            >
             <span class="breadcrumb-item active">Shop Detail</span>
           </nav>
         </div>
